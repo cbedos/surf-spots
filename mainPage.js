c380690191d21d9618ed4fd661cbdffa15032025
cbedos <93485528+cbedos@users.noreply.github.com>
2021-11-02 21:44:29 -0700

    initial commit

---
+++ mainPage.js
@@ -0,0 +1,25 @@
+
+// const el = document.querySelector("#hello");
+// console.log(el);
+
+
+// // LOCATION GRID HOVER FEATURES
+// // (1) grab the image containers
+// const gridImage = document.querySelector(".gridImageContainer"); 
+// console.log(gridImage);
+// // (2) create function that sets background of gridImageContainer to black opaque
+// function blackHover(){
+//     gridImage.style.backgroundColor = "rgba(1,1,1,0.4)";
+// }
+// // (3) add event listner for hover to trigger funtion
+// gridImage.addEventListener("click",blackHover);
+
+
+
+
+// SIDE BAR MENU
+// (1) Grab side bar from DOM
+const sideMenu = document.querySelector("#sideBar");
+
+// (2) Cretae functions to show and hide side menu
+// (2) Add event listener to hamburger 
